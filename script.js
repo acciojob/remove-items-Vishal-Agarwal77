85f18250ad176b165b24612f950284a746ab8327
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-24 18:35:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const item=document.getElementById("colorSelect");
+const all_items=document.getElementsByTagName("option");
+const btn=document.getElementsByTagName("input")[0];
+btn.addEventListener("click",()=>{
+	for(let i of all_items){
+		if(i.innerHTML==item.value){
+			i.remove();
+		}
+	}
+})
